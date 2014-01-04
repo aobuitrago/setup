6db1198286c81f2fbbbfa101c7b7f1f0fc6caf2e
ossian <aobuitrago@gmail.com>
2014-01-04 17:10:44 +0000

    vim config simplified

--- setup.sh
+++ setup.sh
@@ -48,9 +48,4 @@ ln -sb dotfiles/.bash_profile .
 ln -sb dotfiles/.bashrc .
 ln -sb dotfiles/.bashrc_custom .
 ln -sf dotfiles/.emacs.d .
-ln -sb dotfiles/.vimrc .
-
-git clone https://github.com/aobuitrago/vimconfig.git
-mv vimconfig/* ~/.vim/
-rm -rf vimconfig
-
+ln -sf dotfiles/.vim .
